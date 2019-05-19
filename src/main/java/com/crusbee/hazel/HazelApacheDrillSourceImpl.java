8f9d36fd5f16a013feac1e69fee8999aecb71817
Stanislav <stasn@ua.fn>
2019-05-19 23:16:21 +0300

    add queries refactor init data from properties

--- src/main/java/com/crusbee/hazel/HazelApacheDrillSourceImpl.java
+++ src/main/java/com/crusbee/hazel/HazelApacheDrillSourceImpl.java
@@ -14,45 +14,58 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.*;
 
-public class JdbcSource {
+public class HazelApacheDrillSourceImpl {
 
-
-    private static final String QUERY_CATEGORIES = "Select t.cCat.externalId,t.cCat.name.textValue, t.cCat.description.textValue from (select flatten(childCategories) AS cCat FROM dfs.`%s`) t";
-    private static final String QUERY_PRODUCTS = "Select p.product.externalId, p.product.name.textValue, p.product.description.textValue from (SELECT flatten(t.flatdata.products) as product FROM (select flatten(childCategories) AS flatdata FROM dfs.`%s`) t) p";
-    private static final String QUERY_PRODUCTS_SKU = "Select p.product.externalId, p.product.name.textValue from (SELECT flatten(t.flatdata.products) as product FROM (select flatten(childCategories) AS flatdata FROM dfs.`C:\\Users\\Stanislav\\Downloads\\apache_drill\\src\\main\\resources\\catalognew.json`) t) p";
-    private static final String QUERY_PRODUCTS_PARTS = "Select p.product.externalId, p.product.name.textValue from (SELECT flatten(t.flatdata.products) as product FROM (select flatten(childCategories) AS flatdata FROM dfs.`C:\\Users\\Stanislav\\Downloads\\apache_drill\\src\\main\\resources\\catalognew.json`) t) p";
+    private static final String JDBC_DRIVER = "org.apache.drill.jdbc.Driver";
+    private static String CONNECTION;
+    private static final String QUERY_CATEGORIES = "" +
+            "SELECT t.cCat.externalId,t.cCat.name.textValue, t.cCat.description.textValue " +
+            "FROM (SELECT flatten(childCategories) AS cCat FROM dfs.`%s`) t";
+    private static final String QUERY_PRODUCTS = "" +
+            "SELECT p.product.externalId, p.product.name.textValue, p.product.description.textValue " +
+            "FROM (SELECT flatten(t.flatdata.products) as product " +
+            "FROM (SELECT flatten(childCategories) AS flatdata FROM dfs.`%s`) t) p";
+    private static final String QUERY_PRODUCTS_SKU = "" +
+            "SELECT s.sku.defaultProduct, s.sku.name.textValue,s.sku.description.textValue " +
+            "FROM (SELECT flatten(p.product.additionalScus) as sku " +
+            "FROM (SELECT flatten(t.flatdata.products) as product " +
+            "FROM (SELECT flatten(childCategories) AS flatdata FROM dfs.`%s`) t) p) s";
+    private static final String QUERY_PRODUCTS_PARTS = "" +
+            "SELECT parts.part.externalId, parts.part.name.textValue,parts.part.description.textValue " +
+            "FROM (SELECT flatten(p.product.parts) as part " +
+            "FROM (SELECT flatten(t.flatdata.products) as product " +
+            "FROM (SELECT flatten(childCategories) AS flatdata FROM dfs.`%s`) t) p) parts";
     private static final Map<String, String> queriesMap = new HashMap<>();
+
+
     static {
         init();
     }
-
-    private static final String JDBC_DRIVER = "org.apache.drill.jdbc.Driver";
     private JetInstance jet;
     private String dbDirectory;
 
-    public JdbcSource(JetInstance jet, String dbDirectory) {
+    public HazelApacheDrillSourceImpl(JetInstance jet, String dbDirectory) {
         this.jet = jet;
         this.dbDirectory = dbDirectory;
     }
 
     private static void init() {
-        try (InputStream input = JdbcSource.class.getClassLoader().getResourceAsStream("query.properties")) {
+        System.setProperty("hazelcast.logging.type", "log4j");
+        try (InputStream input = HazelApacheDrillSourceImpl.class.getClassLoader().getResourceAsStream("jdbc.properties")) {
             Properties prop = new Properties();
             if (input == null) {
                 System.out.println("Sorry, unable to find config.properties");
                 return;
             }
             prop.load(input);
-            String file = prop.getProperty("query.filepath");
+            String file = prop.getProperty("jdbc.query.filepath");
+            CONNECTION = prop.getProperty("jdbc.connection");
             queriesMap.put("categories", String.format(QUERY_CATEGORIES, file));
             queriesMap.put("products", String.format(QUERY_PRODUCTS, file));
-//        queriesMap.put("productSku", QUERY_PRODUCTS_SKU);
-//        queriesMap.put("productPars", QUERY_PRODUCTS_PARTS);
-
+            queriesMap.put("productSku", String.format(QUERY_PRODUCTS_SKU, file));
+            queriesMap.put("productPars", String.format(QUERY_PRODUCTS_PARTS, file));
         } catch (IOException ex) {
             ex.printStackTrace();
-
-
         }
     }
 
@@ -73,10 +86,9 @@ public class JdbcSource {
         return jet;
     }
 
-    public static void main(String[] args) throws Exception {
-        JdbcSource source = new JdbcSource(Jet.newJetInstance(), Files.createTempDirectory(JdbcSource.class.getName()).toString());
+    public static void main(String[] args) throws IOException {
+        HazelApacheDrillSourceImpl source = new HazelApacheDrillSourceImpl(Jet.newJetInstance(), Files.createTempDirectory(HazelApacheDrillSourceImpl.class.getName()).toString());
         try {
-            System.setProperty("hazelcast.logging.type", "log4j");
             Class.forName(JDBC_DRIVER);
             List<Job> jobs = new ArrayList<>();
             queriesMap.entrySet().forEach(entry -> {
@@ -88,23 +100,21 @@ public class JdbcSource {
             });
             jobs.forEach(Job::join);
             queriesMap.entrySet().forEach(entry -> source.getJet().getMap(entry.getKey()).values().forEach(System.out::println));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         } finally {
-
             source.cleanup();
         }
-
     }
 
     private Pipeline createPipeLine(Map.Entry<String, String> query) throws Exception {
         Pipeline p = buildPipeline(connectionUrl(), query);
         return p;
     }
-
     private void cleanup() {
         Jet.shutdownAll();
     }
-
     private String connectionUrl() {
-        return "jdbc:drill:drillbit=localhost";
+        return CONNECTION;
     }
 }
